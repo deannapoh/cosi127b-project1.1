e33900252cb820b245365f244b7e42b693fad29c
Shubham Kaushik <kaushiks@brandeis.edu>
2026-03-13 09:01:34 -0400

    Updates comments

--- app/routes/queries.py
+++ app/routes/queries.py
@@ -6,7 +6,6 @@ queries_bp = Blueprint("query", __name__)
 
 @queries_bp.route("/list_tables")
 def list_tables():
-    """List all tables in the database."""
 
     # >>>> TODO 1: Write a query to list all the tables in the database. <<<<
 
@@ -19,11 +18,10 @@ def list_tables():
 
 @queries_bp.route("/search_movie", methods=["POST"])
 def search_movie():
-    """Search for movies by name."""
     movie_name = request.form["movie_name"]
 
-    # >>>> TODO 2: Search Motion Picture by Motion picture name. <<<<
-    #              List the `name`, `rating`, `production` and `budget`.
+    # >>>> TODO 2: Search Motion Picture by Motion picture name (parameterized). <<<<
+    #              List the motion picture name along with its rating, production, and budget.
 
     query = """ """
 
@@ -34,10 +32,9 @@ def search_movie():
 
 @queries_bp.route("/search_liked_movies", methods=["POST"])
 def search_liked_movies():
-    """Search for movies liked by a specific user."""
     user_email = request.form["user_email"]
 
-    # >>>> TODO 3: Find the movies that have been liked by a specific user’s email. <<<<
+    # >>>> TODO 3: Find the movies that have been liked by a specific user. A user is uniquely identified by their email (parameterized). <<<<
     #              List the movie `name`, `rating`, `production` and `budget`.
 
     query = """ """
@@ -49,7 +46,6 @@ def search_liked_movies():
 
 @queries_bp.route("/search_by_country", methods=["POST"])
 def search_by_country():
-    """Search for movies by country using the Location table."""
     country = request.form["country"]
 
     # >>>> TODO 4: Search motion pictures by their shooting location country. <<<<
@@ -64,11 +60,10 @@ def search_by_country():
 
 @queries_bp.route("/search_directors_by_zip", methods=["POST"])
 def search_directors_by_zip():
-    """Search for directors and the series they directed by zip code."""
     zip_code = request.form["zip_code"]
 
-    # >>>> TODO 5: List all directors who have directed TV series shot in a specific zip code. <<<<
-    #              List the director name and TV series name only without duplicates.
+    # >>>> TODO 5: List all directors who have directed a TV series shot in a specific zip code (parameterized). <<<<
+    #              List the director’s name and TV series name only, without duplicates.
 
     query = """ """
 
@@ -79,11 +74,10 @@ def search_directors_by_zip():
 
 @queries_bp.route("/search_awards", methods=["POST"])
 def search_awards():
-    """Search for award records where the award count is greater than `k`."""
     k = int(request.form["k"])
 
-    # >>>> TODO 6: Find the people who have received more than “k” awards for a single motion picture in the same year. <<<<
-    #              List the person `name`, `motion picture name`, `award year` and `award count`.
+    # >>>> TODO 6: Identify the individuals who have received more than “k” (parameterized) awards for a single motion picture in the same year. <<<<
+    #              Return the individual’s name, the motion picture’s name, the award year, and the award count.
 
     query = """ """
 
@@ -94,15 +88,11 @@ def search_awards():
 
 @queries_bp.route("/find_youngest_oldest_actors", methods=["GET"])
 def find_youngest_oldest_actors():
-    """
-    Find the youngest and oldest actors based on the difference
-    between the award year and their date of birth.
-    """
 
-    # >>>> TODO 7: Find the youngest and oldest actors to win at least one award. <<<<
-    #              List the actor names and their age (at the time they received the award).
-    #              The age should be computed from the person’s date of birth to the award winning year only.
-    #              In case of a tie, list all of them.
+    # >>>> TODO 7: Find the youngest and oldest actors who have won at least one award. <<<<
+    #              List the actor names and their age at the time they received the award. 
+    #              Age must be computed using the actor’s date of birth and the award-year only. 
+    #              In case of a tie, list all tied actors.
 
     query = """ """
 
@@ -129,14 +119,11 @@ def find_youngest_oldest_actors():
 
 @queries_bp.route("/search_producers", methods=["POST"])
 def search_producers():
-    """
-    Search for American producers based on a minimum box office collection and maximum budget.
-    """
     box_office_min = float(request.form["box_office_min"])
     budget_max = float(request.form["budget_max"])
 
-    # >>>> TODO 8: Find the American [USA] Producers who had a box office collection of more than or equal to “X” with a budget less than or equal to “Y”. <<<<
-    #              List the producer `name`, `movie name`, `box office collection` and `budget`.
+    # >>>> TODO 8: List the American producers whose movies achieved a box office collection greater than or equal to “X” (parameterized) with a budget less than or equal to “Y” (parameterized). <<<<
+    #              List the producer’s name and movie name along with its box office collection and budget.
 
     query = """ """
 
@@ -147,13 +134,10 @@ def search_producers():
 
 @queries_bp.route("/search_multiple_roles", methods=["POST"])
 def search_multiple_roles():
-    """
-    Search for people who have multiple roles in movies with a rating above a given threshold.
-    """
     rating_threshold = float(request.form["rating_threshold"])
 
-    # >>>> TODO 9: List the people who have played multiple roles in a motion picture where the rating is more than “X”. <<<<
-    #              List the person’s `name`, `motion picture name` and `count of number of roles` for that particular motion picture.
+    # >>>> TODO 9: List the individuals who played multiple roles in a motion picture with a rating greater than “X” (parameterized). <<<<
+    #              List the individual’s name, the motion picture name, and the number of roles the individual played in that motion picture.
 
     query = """ """
 
@@ -164,11 +148,10 @@ def search_multiple_roles():
 
 @queries_bp.route("/top_thriller_movies_boston", methods=["GET"])
 def top_thriller_movies_boston():
-    """Display the top 2 thriller movies in Boston based on rating."""
 
-    # >>>> TODO 10: Find the top 2 rates thriller movies (genre is thriller) that were shot exclusively in Boston. <<<<
-    #               This means that the movie cannot have any other shooting location.
-    #               List the `movie names` and their `ratings`.
+    # >>>> TODO 10: Find the top 2 highest-rated thriller movies (genre: thriller) that were shot exclusively in Boston. <<<<
+    #               “Exclusively” means that the movie may not have any other shooting location. 
+    #               List the movie names and their rating.
 
     query = """ """
 
@@ -179,15 +162,11 @@ def top_thriller_movies_boston():
 
 @queries_bp.route("/search_movies_by_likes", methods=["POST"])
 def search_movies_by_likes():
-    """
-    Search for movies that have received more than a specified number of likes,
-    where the liking users are below a certain age.
-    """
     min_likes = int(request.form["min_likes"])
     max_age = int(request.form["max_age"])
 
-    # >>>> TODO 11: Find all the movies with more than “X” likes by users of age less than “Y”. <<<<
-    #               List the movie names and the number of likes by those age-group users.
+    # >>>> TODO 11: Find all the movies with more than “X” (parameterized) likes by users of age less than “Y” (parameterized). <<<<
+    #               Return the movie names and the number of likes from that age group.
 
     query = """ """
 
@@ -198,12 +177,9 @@ def search_movies_by_likes():
 
 @queries_bp.route("/actors_marvel_warner", methods=["GET"])
 def actors_marvel_warner():
-    """
-    List actors who have appeared in movies produced by both Marvel and Warner Bros.
-    """
 
-    # >>>> TODO 12: Find the actors who have played a role in both “Marvel” and “Warner Bros” productions. <<<<
-    #               List the `actor names` and the corresponding `motion picture names`.
+    # >>>> TODO 12: Identify the actors who played a role in both “Marvel” and “Warner Bros” productions. <<<<
+    #               List the actor names and the corresponding motion picture names.
 
     query = """ """
 
@@ -214,12 +190,9 @@ def actors_marvel_warner():
 
 @queries_bp.route("/movies_higher_than_comedy_avg", methods=["GET"])
 def movies_higher_than_comedy_avg():
-    """
-    Display movies whose rating is higher than the average rating of comedy movies.
-    """
 
-    # >>>> TODO 13: Find the motion pictures that have a higher rating than the average rating of all comedy (genre) motion pictures. <<<<
-    #               Show the names and ratings in descending order of ratings.
+    # >>>> TODO 13: Find the motion pictures with a higher rating than the average rating of all comedy (genre) motion pictures.  <<<<
+    #               List the names and ratings, sorted in descending order of ratings.
 
     query = """ """
 
@@ -246,9 +219,6 @@ def movies_higher_than_comedy_avg():
 
 @queries_bp.route("/actors_with_common_birthday", methods=["GET"])
 def actors_with_common_birthday():
-    """
-    Find pairs of actors who share the same birthday.
-    """
 
     # >>>> TODO 14: Find actors who share the same birthday. <<<<
     #               List the actor names (actor 1, actor 2) and their common birthday.
@@ -262,9 +232,7 @@ def actors_with_common_birthday():
 
 @queries_bp.route("/top_production_by_genre", methods=["GET"])
 def top_production_by_genre():
-    """
-    Display the top production companies for each genre based on the number of movies with above-average ratings.
-    """
+
     # >>>> TODO 15: List the productions that have produced more than two movies in a given genre, where each movie has a rating higher than the average rating of that genre. <<<<
     #               List the `production company name`, `genre name` and the `count of movies` that meet the criteria, ordered by the count of movies in descending order.
 
@@ -279,10 +247,8 @@ def top_production_by_genre():
 
 @queries_bp.route("/versatile_talent", methods=["GET"])
 def versatile_talent():
-    """
-    Display people who have won awards for acting, directing, and producing.
-    """
-    # >>>> TODO 16: Find the people who have won awards and performed as an actor, director, and producer. <<<<
+
+    # >>>> TODO 16: Find individuals who have acted, directed, and produced motion pictures, and have won at least one award against one of those roles. <<<<
     #               List the person’s `name` and `nationality`.
 
     query = """ """
@@ -298,10 +264,8 @@ def versatile_talent():
 
 @queries_bp.route("/high_roi_movies", methods=["GET"])
 def high_roi_movies():
-    """
-    Display the top 5 movies shot in the USA with the highest return on investment (ROI) compared to the average ROI of Marvel movies.
-    """
-    # >>>> TODO 17: Find the top 5 movies shot in the USA with the highest return on investment (ROI), where ROI is defined as box office collection divided by budget. <<<<
+
+    # >>>> TODO 17: Find the top 5 movies produced(shooted) in the USA with a “Return on Investment” (Box Office/Budget) higher than the average return on investment of all Marvel movies. <<<<
     #               Only include movies that have an ROI greater than the average ROI of all Marvel movies
     #               First column should be the movie name, second column should be country, and third column should be the ROI.
 
@@ -334,10 +298,8 @@ def high_roi_movies():
 
 @queries_bp.route("/awarded_series_growth", methods=["GET"])
 def awarded_series_growth():
-    """
-    Display TV series that have won awards and have a season count above the average, ordered by season count in descending order.
-    """
-    # >>>> TODO 18: Find the TV series that have won at least one award and have a season count above the average season count of all TV series. <<<<
+
+    # >>>> TODO 18: Find all TV series that have more seasons than the average season count of all series, and have at least one award-winning person after the year 2010. <<<<
     #               List the TV series `name`, `season count` and the `number of awards won`, ordered by season count in descending order.
 
     query = """ """
